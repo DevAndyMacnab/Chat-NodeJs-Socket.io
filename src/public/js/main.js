28f4d49de7aa9b57786c530b793f5a0164b1ce07
DevAndyMacnab <andy.macnab.j@gmail.com>
2022-12-01 13:28:13 -0600

    registro usuario funcionando

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,10 +1,15 @@
 $(function () {
   const socket = io();
-
+  var nick = "";
   //Accedemos a los elementos del DOM:
   const messageForm = $("#message-form");
   const messageBox = $("#message");
   const chat = $("#chat");
+
+  const nickForm = $("#nick-form");
+  const nickError = $("#nick-error");
+  const nickName = $("#nick-name");
+  const usernames = $("#usernames");
 
   //Eventos
   //Enviamos un mensaje al servidor
@@ -13,4 +18,31 @@ $(function () {
     socket.emit("enviar mensaje", messageBox.val());
     messageBox.val("");
   });
+
+  socket.on("nuevo mensaje", function (datos) {
+    chat.append(
+      `<div class="msg-area mb-2"><p class="msg">${datos.msg}</p></div>`
+    );
+  });
+
+  //Nuevo usuario:
+  nickForm.submit((evento) => {
+    evento.preventDefault();
+    console.log("si funciona");
+    socket.emit("nuevo usuario", nickName.val(), (datos) => {
+      if (datos) {
+        nick = nickName.val();
+        $("#nick-wrap").hide();
+        $("#content-wrap").show();
+      } else {
+        nickError.html(
+          '<div class="alert alert-danger">El usuario ya existe</div>'
+        );
+      }
+      console.log("Llegamos por aqui");
+      nickName.val("");
+    });
+  });
 });
+
+//Obtenemos respuesta del servidor
--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,32 @@
-module.exports = (io)=>{
-    io.on("connection",socket=>{
-        console.log("Nuevo usuario conectado")
+module.exports = (io) => {
+    //En lugar de un arreglo se puede sustituir por una base de datos
+    let nickNames=[];
+  io.on("connection", (socket) => {
+    console.log("Nuevo usuario conectado");
 
-        //Al recibir un mensaje recojemos los datos:
-        socket.on('enviar mensaje',(datos)=>{
-            console.log(datos);
-        })
+    //Al recibir un mensaje recojemos los datos:
+    socket.on("enviar mensaje", (datos) => {
+      console.log(datos);
+      io.sockets.emit("nuevo mensaje", {
+        msg: datos,
+      });
+    });
 
-    })
-}
+    socket.on("nuevo usuario", (datos, callback) => {
+        console.log("anda por aca")
+        if(nickNames.indexOf(datos)!=-1){
+            callback(false);
+
+        }else{
+            callback(true);
+            socket.nickname=datos;
+
+            nickNames.push(socket.nickname);
+
+            io.sockets.emit("usernames", nickNames);
+
+        }
+
+    });
+  });
+};
