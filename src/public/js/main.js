77ce63db8ee48306c52f21765f993a501e36d0bc
DevAndyMacnab <andy.macnab.j@gmail.com>
2022-11-30 18:45:52 -0600

    agregando jquery

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,16 @@
-const socket= io();
+$(function () {
+  const socket = io();
+
+  //Accedemos a los elementos del DOM:
+  const messageForm = $("#message-form");
+  const messageBox = $("#message");
+  const chat = $("#chat");
+
+  //Eventos
+  //Enviamos un mensaje al servidor
+  messageForm.submit((evento) => {
+    evento.preventDefault();
+    socket.emit("enviar mensaje", messageBox.val());
+    messageBox.val("");
+  });
+});
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,11 @@
 module.exports = (io)=>{
     io.on("connection",socket=>{
         console.log("Nuevo usuario conectado")
+
+        //Al recibir un mensaje recojemos los datos:
+        socket.on('enviar mensaje',(datos)=>{
+            console.log(datos);
+        })
+
     })
 }
